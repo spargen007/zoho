f5dc18b902987b5a85247f0b0886b08b20085fdb
spargen-pt6214 <spargen-pt6214@10.51.5.118>
2022-10-03 17:41:24 +0530

    sorting

---
+++ sortsql/src/sortsql/Sorting.java
@@ -0,0 +1,27 @@
+package sortsql;
+import java.sql.*;
+public class Sorting {
+	static final String url = "jdbc:mysql://localhost/School";
+    static final String user = "root";
+	static final String pass = "Spargen007";
+	public static void main(String args[]) {
+		try {
+			Connection con=DriverManager.getConnection(url,user,pass);
+			Statement stmt=con.createStatement();
+			String sql="SELECT * FROM Students ORDER BY Regno desc";
+			ResultSet r=stmt.executeQuery(sql);
+			while(r.next()) {
+	            System.out.print("Regno: " + r.getInt("Regno")+"\n");
+	            System.out.print("Name: " + r.getString("Name")+"\n");
+	            System.out.print("dob: "+r.getDate("DOB")+"\n");
+	            System.out.print("Age: " + r.getInt("AGE")+"\n");
+	            System.out.print("Gender: " + r.getString("Gender")+"\n");
+	            System.out.println("phone: "+r.getString("Phone")+"\n");
+	            System.out.println();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
